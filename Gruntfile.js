3b4051f4e74f8aefef1eecd705eceea8414bad4f
John-Philip Johansson <seriema@gmail.com>
2014-03-03 11:21:18 +0100

    Add Livereload support

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,18 @@ module.exports = function(grunt) {
 	// Project configuration.
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+
+		connect: {
+			server: {
+				options: {
+					port: 9001,
+					base: 'webpage',
+					livereload: true,
+					open: true
+				}
+			}
+		},
+
 		'gh-pages': {
 			options: {
 				base: 'webpage',
@@ -17,13 +29,30 @@ module.exports = function(grunt) {
 					"webpage/css/font-awesome-colors.css": "webpage/less/font-awesome-colors.less"
 				}
 			}
+		},
+
+		watch: {
+			less: {
+				files: ['webpage/less/*.less'],
+				tasks: ['less']
+			},
+			statics: {
+				files: ['webpage/*.html', 'webpage/js/*.js', 'webpage/fonts/*', 'webpage/img']
+			},
+			livereload: {
+				options: { livereload: true },
+				files: ['webpage/**/*.*']
+			}
 		}
 	});
 
+	grunt.loadNpmTasks('grunt-contrib-connect');
 	grunt.loadNpmTasks('grunt-gh-pages');
 	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
 	// Default task(s).
 	grunt.registerTask('default', ['less']);
 
+	grunt.registerTask('serve', ['connect', 'watch']);
 };
